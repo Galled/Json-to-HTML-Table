003a283606558ba2edac1c6237a9c683793ea34b
Galled <arianedune@gmail.com>
2011-09-25 00:10:23 -0500

    Add boolean parameters

--- json-to-table.js
+++ json-to-table.js
@@ -1,21 +1,32 @@
-/*
-** Convert Json Data to HTML Table 
-** @author Afshin Mehrabani <afshin dot meh at gmail dot com>
-*/ 
-
-//function ConvertJsonToTable(jsonData, keys, containerId, tableClassName) {
-
+/******************************************************************
+Optional: php.js --> http://phpjs.org/functions/array_keys:324
+******************************************************************/
 function json2HtmlTable(arrParams) {
 /******************************************************************
 Input:
 arrParams = {
-    arrJsonData		: array,
+	arrJsonData		: array,
 	arrKeys			: array, 
-	arrTableAttr	: array
+	arrTableAttr	: array,
+	bOnlyHead		: boolean [false],
+	bOnlyBody		: boolean [false],
+	bNoTableWrap	: boolean [false]
 }
 example:
 arrParams = {
-	arrJsonData		: {"COD_PRODUCTO":["00000000000000000002"],"C&oacute;digo":[""],"Descripci&oacute;n":["Sastre manga 3\/4"],"Categor&iacute;a":["Sastres"],"Colecci&oacute;n":["OTONIO 2010"],"Modelo":["DANIELLA"],"Talla":["L"],"Color":["AZUL MARINO"],"COLORHEX":["#0000ff"],"Tela":[""],"Vendido":["29"]},
+	arrJsonData		: {
+						"COD_PRODUCTO":["00000000000000000002"],
+						"C&oacute;digo":[""],
+						"Descripci&oacute;n":["Sastre manga 3\/4"],
+						"Categor&iacute;a":["Sastres"],
+						"Colecci&oacute;n":["OTONIO 2010"],
+						"Modelo":["DANIELLA"],
+						"Talla":["L"],
+						"Color":["AZUL MARINO"],
+						"COLORHEX":["#0000ff"],
+						"Tela":[""],
+						"Vendido":["29"]
+					},
 	arrKeys			: ['C&oacute;digo','Descripci&oacute;n','Categor&iacute;a','Colecci&oacute;n','Modelo','Talla', 'Color','Tela','Vendido'], 
 	arrTableAttr	: {
 		'id'	: 'myTableId'
@@ -24,7 +35,7 @@ arrParams = {
 
 ********************************************************************/ 
     //Patterns for table thead & tbody
-    var sTable = '<table {0} >',
+    var sTable = "",
 	th = "<thead>{0}</thead>",
 	tb = "<tbody>{0}</tbody>",
 	tr = "<tr>{0}</tr>",
@@ -33,7 +44,10 @@ arrParams = {
 	thCon = "",
 	tbCon = "",
 	trCon = "",
-	sAttr = '',arrTableAttr;
+	sAttr = '', arrTableAttr, 
+	bOnlyHead 		= (typeof arrParams['bOnlyHead'] !== 'undefined') ? arrParams['bOnlyHead'] : false,
+	bOnlyBody 		= (typeof arrParams['bOnlyBody'] !== 'undefined') ? arrParams['bOnlyBody'] : false,
+	bNoTableWrap 	= (typeof arrParams['bNoTableWrap'] !== 'undefined') ? arrParams['bNoTableWrap'] : false;
 	
 	
 	String.prototype.format = function(sParam1,sParam2){
@@ -47,36 +61,47 @@ arrParams = {
 			sAttr += sTableAttr+'="'+arrTableAttr[sTableAttr]+'"';
 		}
 	}
+	if(bNoTableWrap===false){
+		sTable += '<table {0} >';
+		sTable = sTable.format(sAttr);
+		sTable += '{0}{1}</table>';
+	}
+	console.warn('sTable:',sTable,' bNoTableWrap:',bNoTableWrap);
 	
-	sTable = sTable.format(sAttr) + '{0}{1}</table>';
-	
-    if (typeof arrParams['arrJsonData']!='undefined' && typeof arrParams['arrKeys']!='undefined') {
+    if (typeof arrParams['arrJsonData'] !== 'undefined') {
         //Creating all table headers			
-		var arrKeys = arrParams['arrKeys'],
+		var arrJsonData = arrParams['arrJsonData'],
+		arrKeys = typeof arrParams['arrKeys'] !== 'undefined' 
+					? arrParams['arrKeys'] 
+					: (
+						(typeof window.array_keys == 'function')
+							? array_keys(arrJsonData) // http://phpjs.org/functions/array_keys:324
+							: []
+						),
 		nMax = arrKeys.length,
-		nI = nMax,
-		nJ, sTempHead,
-		arrJsonData = arrParams['arrJsonData'],
-		nMax0, nK, nL;
-		if(typeof arrJsonData[arrKeys[0]]!='undefined'){
+		nI = nMax, nJ, sTempHead, nMax0, nK, nL;
+		console.log(arrKeys);
+		if(typeof arrJsonData[arrKeys[0]] !== 'undefined'){
 			nMax0 = arrJsonData[arrKeys[0]].length;
-			for (; nI--;) {
-				nJ = nMax - nI - 1;			
-				thCon += thRow.format(arrKeys[nJ]);
-			}
-			nK = nMax0;
-			for (; nK--;) {
-				nL = nMax0 - nK - 1;
-				nI = nMax;
+			if(bOnlyBody===false){
 				for (; nI--;) {
-					if(nI<0){
-						break;
-					}
-					nJ = nMax - nI - 1;
-					tbCon += tdRow.format(arrJsonData[arrKeys[nJ]][nL]);
+					nJ = nMax - nI - 1;			
+					thCon += thRow.format(arrKeys[nJ]);
 				}
-				trCon += tr.format(tbCon);
-				tbCon = "";
+			}
+			if(bOnlyHead===false){
+				nK = nMax0;
+				for (; nK--;) {
+					nL = nMax0 - nK - 1;
+					nI = nMax;
+					for (; nI--;) {
+						if(nI<0){ break; }
+						nJ = nMax - nI - 1;
+						tbCon += tdRow.format(arrJsonData[arrKeys[nJ]][nL]);
+					}
+					trCon += tr.format(tbCon);
+					tbCon = "";
+				}
 			}
 			
 			th = th.format(tr.format(thCon));
